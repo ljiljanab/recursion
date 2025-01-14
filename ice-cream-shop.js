95d95b90b5ad3f83cdba1d3670daeace3cbf121c
ljiljanab <ljiljana.qdz@gmail.com>
2025-01-14 21:50:48 +0100

    recursive func

---
+++ ice-cream-shop.js
@@ -0,0 +1,19 @@
+/*
+Write a recursive function `iceCreamShop(flavors, favorite)` that takes in an
+array of ice cream flavors available at the ice cream shop, as well as the
+user's favorite ice cream flavor. Recursively find out whether or not the shop
+offers their favorite flavor.
+*/
+
+const iceCreamShop = (flavors, favorite) => {
+    
+    if (flavors.length == 0 ) return false;
+    if (flavors[0] === favorite) return true;
+    return iceCreamShop(flavors.slice(1), favorite);
+}
+
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); //false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+
+console.log(iceCreamShop(['cookes n cream', 'blue moon', 'superman', 'honey lavender'], 'pistachio')); // false
+console.log(iceCreamShop([], 'lavender')); //false
